5cf939c4e85d59a5558536c41b121da3c23924fb
RIKKEI_ニー <nhitny@rikkeisoft.com>
2025-09-15 22:56:21 +0900

    video_array

---
+++ lesson02-data-structure/src/video_array/Ex02Filter.java
@@ -0,0 +1,36 @@
+package video_array;
+
+import java.util.Arrays;
+
+/*
+  1.tìm những phần tử lẻ trong mảng số nguyên
+    Input: mảng
+    output: mảng số lẻ
+  
+ */
+public class Ex02Filter {
+	public static void main(String[] args) {
+		int[] numbers = {23,25,16,33,66};
+		int[] oddNumbers = findOddNumbers(numbers);
+		System.out.println(Arrays.toString(oddNumbers));
+		
+		
+	}
+	// int[] elements = numbers;
+	private static int[] findOddNumbers(int[] elements) {
+		 int[] oddElements = new int[elements.length];
+		 int count = 0;
+		for (int element: elements) {
+			if (element % 2 != 0) {
+				oddElements[count] = element;
+				count++;
+			}
+		} 
+		int[] result = new int[count];
+		for (int i = 0; i < count; i++) {
+			result[i] = oddElements[i];
+		}
+		return result;
+	}
+
+}
