0e517b6eba8879c18c31feaa64021f0b1da100b1
NguyenVanTai010105 <nvt112005@gmail.com>
2025-09-15 22:56:09 +0700

    Add workspace-NguyenVanTai

---
+++ lession01-setup-env/src/view/Application.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.time.format.DateTimeFormatter;
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.print("Hahaha11");
+		System.out.println("WElcome to Eclipse 2024" + DateTimeFormatter.ofPattern("dd/mm/yy"));
+	}
+	/*
+	 * 
+	 * 
+	 */
+}
