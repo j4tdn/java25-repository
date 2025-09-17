149aff452b650723ab35f2c51ef1fa7ee699e8c1
HuyPham05 <huyphamcs456@gmail.com>
2025-09-17 22:39:41 +0700

    lesson03

---
+++ lession03-oop/src/bean/CarAdv.java
@@ -0,0 +1,36 @@
+package bean;
+
+public class CarAdv {
+	
+	private static String model;
+	private String color;
+	
+	public CarAdv() {
+	}
+
+	public CarAdv(String color) {
+		this.color = color;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public static void setModel(String model) {
+		CarAdv.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "CarAdv [model=" + model + ", color=" + color + "]";
+	}
+
+}
