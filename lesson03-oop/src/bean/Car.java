123f976da9f4cbf585e1ae0f93c34225d942bf0e
quantrantk4 <quantrantk4@gmail.com>
2025-09-25 22:53:50 +0700

    workspace-tranminhquan: 3 lesson 25.09.2025

---
+++ lesson03-oop/src/bean/Car.java
@@ -0,0 +1,101 @@
+package bean;
+
+//  Comment 1 hàng
+/*  Comment N hàng ở bất kỳ vị trí nào*/
+/** Comment trước class/method để giá trị mô tả class/method đó */
+
+/**
+ * Class Car có các thuộc tính
+ * Từ class Car có thể tạo ra các đối tượng
+ * 
+ * Class Car --> Kiểu Dữ Liệu
+ */
+public class Car {
+	
+	// attribute(s) = thuộc tính = biến toàn cục
+	// sẽ được khởi tạo, gán giá trị khi một đối tượng được tạo ra
+	// cứ mỗi đối tượng nó sẽ quản lý riêng các thuộc tính(giá trị) của riêng nó
+	private Integer id;
+	private String model;
+	private String name;
+	private Double salesPrice;
+	private Integer amount;
+	
+	// theo hướng phát triển oop
+	// mỗi class sẽ quản lý thuộc tính riêng của class đó --> thuộc tính trong class phải là private
+	// trong thực tế dự án các class sẽ gọi qua về để lấy, cập nhật dữ liệu để xử lý yêu cầu của bài
+	// thay vì truy cập trực tiếp các thuộc tính của class
+	// class hỗ trợ getter/setter[public] để lấy/cập nhật giá trị cho từng thuộc tính private
+	
+	// từ nay về sau
+	// khi tạo 1 class thì các thuộc tính phải cho là private và thêm getter, setter
+	// thứ tự: thuộc tính, hàm khởi tạo, getter-setter, toString
+	
+	// nếu để thuộc tính là public thì vẫn có thể truy cập trực tiếp từ bên ngoài giống như fop
+	
+	// default constructor
+	// hàm khởi tạo mặc định, dùng để khởi tạo đối tượng
+	public Car() {
+	}
+
+	// constructor
+	public Car(Integer id, String model, String name, Double salesPrice) {
+		this.id = id;
+		this.model = model;
+		this.name = name;
+		this.salesPrice = salesPrice;
+		this.amount = 1;
+	}
+	
+	// class #
+	// Car c2 = new Car(2, "honda", "hrv", 888d);
+	// c2.amount = 6;
+	// c2.salesPrice = 999d; -- c2.setSalesPrice(999)
+	
+	// sout(c2.salesPrice) -- sout(c2.getSalesPrice())
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalesPrice() {
+		return salesPrice;
+	}
+
+	public void setSalesPrice(Double salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", model=" + model + ", name=" + name + ", salesPrice=" + salesPrice + ", amount="
+				+ amount + "]";
+	}
+}
---
+++ lesson03-oop/src/view/Ex03StaticAttribute.java
@@ -0,0 +1,43 @@
+package view;
+
+import bean.CarAdv;
+
+public class Ex03StaticAttribute {
+	
+	public static void main(String[] args) {
+		
+		// tạo 100 mẫu xe Audi với các màu sắc khác nhau
+		CarAdv.setModel("Audi");
+		
+		CarAdv c1 = new CarAdv("Red");
+		CarAdv c2 = new CarAdv("Green");
+		CarAdv c3 = new CarAdv("Blue");
+		CarAdv c4 = new CarAdv("Orange");
+		CarAdv c5 = new CarAdv("Purple");
+		CarAdv ck = new CarAdv("White");
+		// ....
+		CarAdv c99 = new CarAdv("Black");
+		CarAdv c100 = new CarAdv("Pink");
+		
+		// chủ sở hữu đổi tên hãng xe, không phải là Audi mà sang tên khác MG
+		
+		// nếu model là non-static thì mỗi đối tượng sẽ quản lý riêng thuộc tính của đối tượng đó
+		// đổi model của c2 thì chỉ ô nhớ của c2 bị thay đổi, còn lại vẫn giữ nguyên
+		
+		
+		CarAdv.setModel("MG");
+		
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		System.out.println("c3: " + c3);
+		System.out.println("c4: " + c4);
+		System.out.println("c5: " + c5);
+		System.out.println("ck: " + ck);
+		System.out.println("c99: " + c99);
+		System.out.println("c100: " + c100);
+		
+
+		
+	}
+	
+}
