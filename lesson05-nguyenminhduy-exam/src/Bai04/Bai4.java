eadf565986fd061fa4f926e1e365a756192f333f
minhduy <duyminh0935@gmail.com>
2025-11-09 00:02:41 +0700

    lesson05-nguyenminhduy 08.11.2025

--- lesson05-nguyenminhduy-exam/src/Bai04/Bai4.java
+++ lesson05-nguyenminhduy-exam/src/Bai04/Bai4.java
@@ -2,4 +2,18 @@ package Bai04;
 
 public class Bai	4 {
 
+	public static int[] getNumbers(int[] arr) {
+		int n = arr.length;
+		int count = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int sum = 0;
+			for(int j = 0; j < n; j++) {
+				if(arr[i] == arr[j]) {
+					sum++;
+				}
+				if(sum == 1) count++;
+			}
+		}
+	}
 }
