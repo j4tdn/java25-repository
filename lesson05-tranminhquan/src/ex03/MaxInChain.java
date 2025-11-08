8d1b502ac3bf359f8aec021af6d9e9516c93a3ac
quantrantk4 <quantrantk4@gmail.com>
2025-11-08 22:35:53 +0700

    Lesson05-tranminhquan 11.08.2025

---
+++ lesson05-tranminhquan/src/ex03/MaxInChain.java
@@ -0,0 +1,17 @@
+package ex03;
+
+import java.util.Scanner;
+
+public class MaxInChain {
+
+	public static int getMaxValidNumber(String s) {
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập chuỗi: ");
+		String s = sc.nextLine();
+		
+	}
+}
