95580799a20769ec9d9f09190309acefdad2b456
Quyen Phan <Quyen.Phan@mgm-tp.com>
2025-12-14 09:10:05 +0700

    lesson07-libraries 13.12.2025

---
+++ lesson07-libraries/src/utils/DateTimeUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class DateTimeUtils {
+	
+	private DateTimeUtils() {
+	}
+	
+	public static String getUnitValue(long value, String unit) {
+		if (value != 0) {
+			String plural = value > 1 ? "s " : " ";
+			return value + " " + unit + plural; 
+		}
+		return "";
+	}
+}
