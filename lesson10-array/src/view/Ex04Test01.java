fd1a33bfc27f35cd722d15ee59906147a1163441
Quyen Phan <Quyen.Phan@mgm-tp.com>
2026-01-18 08:56:23 +0700

    lesson10-array 17.01.2026

---
+++ lesson10-array/src/view/Ex04Test01.java
@@ -0,0 +1,16 @@
+package view;
+
+public class Ex04Test01 {
+	
+	/*
+	 * Cho một mảng số nguyên gồm N phần tử
+	 * Viết chương trình
+	 * + Tìm các phần tử chẵn trong mảng
+	 * + Tìm các phần tử là số nguyên tố trong mảng
+	 * + Tìm các phần tử là bội của 5 trong mảng
+	 * 
+	 * 1: Mỗi yêu cầu viết 1 hàm để xử lý
+	 * 2: Viết duy nhất 1 hàm để xử lý cho 3 yêu cầu
+	 */
+	
+}
---
+++ lesson10-array/src/view/Ex05Test02.java
@@ -0,0 +1,17 @@
+package view;
+
+public class Ex05Test02 {
+	
+	/*
+	 Cho một mảng số nguyên gồm N phần tử
+	 Viết chương trình
+	 + Tìm tổng các phần tử trong mảng
+	 + Tìm tích các phần tử trong mảng
+	 + Tìm giá trị lớn nhất trong mảng
+	 + Tìm giá trị nhỏ nhất trong mảng
+	 
+	 1: Mỗi yêu cầu viết 1 hàm để xử lý
+	 2: Viết duy nhất 1 hàm để xử lý cho 4 yêu cầu trên
+	 */
+	
+}
